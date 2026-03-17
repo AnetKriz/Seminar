cb2daf28666dbb716e8f284b0dd1a8c9a4df5986
AnetKriz <aneta.krizova6@gmail.com>
2026-03-17 16:46:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,53 +1,51 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-
-namespace prvocislo
+﻿namespace test_na_programovani
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> Cisla = new List<int>();
-            List<int> Prvocisla = new List<int>();
-            int nula = 0;
-           while(true) {
-                int n = Convert.ToInt32(Console.ReadLine());
-                Cisla[nula] = n;
-                nula++;
-                if (n == 0) {
-                    break;
+            int pocetMest = Convert.ToInt32(Console.ReadLine());
+
+            int[,] maticeSouvislostiMest = new int[pocetMest, pocetMest];
+
+            for (int i = 0; i < pocetMest; i++)
+            {
+                int[] dvojiceMest = Convert.ToInt32(Console.ReadLine().Split());
+
+                int x = dvojiceMest[i];
+                int y = dvojiceMest[i + 1];
+                maticeSouvislostiMest[x, y] = 1;
+                maticeSouvislostiMest[y, x] = 1;
+
+            }
+
+            int pocatek = Convert.ToInt32(Console.ReadLine());
+            int cil = Convert.ToInt32(Console.ReadLine());
+
+            if  (maticeSouvislostiMest[pocatek, cil] == 1)
+
+            {
+                Console.WriteLine($"{pocatek} {cil}");
+            }
+            Queue<int> cesta = new Queue<int>();
+            cesta.Enqueue(pocatek);
+            for (int i = 0;i < pocetMest; i++)
+            {
+                if (maticeSouvislostiMest[pocatek, i] == 1)
+                {
+                    cesta.Enqueue(i);
+                    pocatek = i;
+                    i = 0;
                 }
-
-
+                else
+                {
+                    continue;
+                }
             }
 
-            for (int i = 0; i< Cisla.Count;i++) {
-                if JePrvocislo(Cisla[i]) = 0;
-                Prvocisla.Add(Cisla[i]);
-            }
+            
 
-            Console.WriteLine(Prvocisla);
-
-            if Prvocisla.Count == 0 {
-                Console.WriteLine("V teto posloupnosti neni žádné prvočíslo");
-            }
+            
         }
     }
-
-    }
-        
-        public class JePrvocislo
-        {
-            public JePrvocislo(int n )
-            {
-              for (int i = 2; i < n; i++) {
-                       int zbytek = n % i;
-                    if (zbytek == 0) {
-                        return zbytek;
-                    }
-                }
-            }
 }
-
